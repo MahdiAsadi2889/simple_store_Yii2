b404ca1c38af529bc8af8176b99e8a633d641e18
MahdiAsadi2889 <mahdi.asadi2889@gmail.com>
2026-07-07 16:04:10 +0330

    initialize RbacController

--- commands/RbacController.php
+++ commands/RbacController.php
@@ -3,8 +3,43 @@
 namespace app\commands;
 
 use yii\console\Controller;
+use Yii;
+use yii\rbac\Role;
+use yii\rbac\Permission;
 
 class RbacController extends Controller
 {
-    
+    public function actionInit()
+    {
+        $auth = Yii::$app->authManager;
+
+        // حذف اطلاعات قبلی 
+        $auth->removeAll();
+
+        // ساخت Role ها 
+        $admin = $auth->createRole('admin');
+        $auth->add($admin);
+
+        $customer = $auth->createRole('customer');
+        $auth->add($customer);
+
+        // ساخت Persmission ها 
+        $productView = $auth->createPermission('product/view');
+        $productView->description = 'View Products';
+        $auth->add($productView);
+
+        $productCreate = $auth->createPermission('product/create');
+        $productCreate->description = 'Create Products';
+        $auth->add($productCreate);
+
+        $productUpdate = $auth->createPermission('product.update');
+        $productUpdate->description = 'Update Products';
+        $auth->add($productUpdate);
+
+        $productDelete = $auth->createPermission('product.delete');
+        $productDelete->description = 'Delete Products';
+        $auth->add($productDelete);
+
+        $this->stdout("RBAC initialized successfully.\n");
+    }
 }
