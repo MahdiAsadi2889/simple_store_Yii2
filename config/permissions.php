b615a67e1a5425831376a9cfb3993502baf13fc2
MahdiAsadi2889 <mahdi.asadi2889@gmail.com>
2026-07-18 11:40:33 +0330

    Add "role" Permissions

--- config/permissions.php
+++ config/permissions.php
@@ -1,13 +1,23 @@
 <?php
 
 return [
+
+    // Product
     'product/view',
     'product/create',
     'product/update',
     'product/delete',
 
+    // User
     'user/view',
     'user/create',
     'user/update',
-    'user/delete'
+    'user/delete',
+
+    // Role
+    'role/view',
+    'role/create',
+    'role/update',
+    'role/delete',
+
 ];
