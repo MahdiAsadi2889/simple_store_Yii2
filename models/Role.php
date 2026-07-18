e74e9eaea376dee4ef5e5ca3b6646639a2facb29
MahdiAsadi2889 <mahdi.asadi2889@gmail.com>
2026-07-18 11:38:37 +0330

    Add Method to Role

--- models/Role.php
+++ models/Role.php
@@ -2,6 +2,7 @@
 
 namespace app\models;
 
+use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 
 class Role extends ActiveRecord
@@ -9,6 +10,13 @@ class Role extends ActiveRecord
     public static function tableName(): string
     {
         return '{{%role}}';
+    }
+
+    public function behaviors(): array
+    {
+        return [
+            TimestampBehavior::class,
+        ];
     }
 
     public function rules(): array
@@ -19,6 +27,18 @@ class Role extends ActiveRecord
             [['created_at', 'updated_at'], 'integer'],
             [['name'], 'string', 'max' => 50],
             [['name'], 'unique'],
+            [['name'], 'trim'],
+        ];
+    }
+
+    public function attributeLabels(): array
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
         ];
     }
 
@@ -29,6 +49,6 @@ class Role extends ActiveRecord
 
     public function getUsers()
     {
-        return $this->hasMany(User::class, ['id'=> 'user_id'])->viaTable('user_role', ['role_id' => 'id']);
+        return $this->hasMany(User::class, ['id' => 'user_id'])->viaTable('user_role', ['role_id' => 'id']);
     }
 }
