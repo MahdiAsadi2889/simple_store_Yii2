4ede77fc4a5eaf70eed9eb60245e26c43e6e04a8
MahdiAsadi2889 <mahdi.asadi2889@gmail.com>
2026-07-25 11:40:13 +0330

    Add Methods for User Roles, Direct Permissions and Effective Permissions

--- services/UserService.php
+++ services/UserService.php
@@ -2,7 +2,10 @@
 
 namespace app\services;
 
+use app\models\Role;
+use app\models\RolePermission;
 use app\models\User;
+use app\models\UserPermission;
 use yii\data\ActiveDataProvider;
 use yii\web\NotFoundHttpException;
 
@@ -31,5 +34,44 @@ class UserService
         }
 
         return $user->save(false);
+    }
+
+    public function getUserRoles(User $user): array
+    {
+        return Role::find()
+            ->innerJoin(
+                'user_role',
+                'user_role.role_id = role.id'
+            )
+            ->where(['user_role.user_id' => $user->id])
+            ->all();
+    }
+
+    public function getUserDirectPermissions(User $user): array
+    {
+        return UserPermission::find()
+            ->select(['permission'])
+            ->where(['user_id' => $user->id])
+            ->column();
+    }
+
+    public function getUserEffectivePermissions(User $user): array
+    {
+        $permissions = $this->getUserDirectPermissions($user);
+
+        $rolePermissions = RolePermission::find()
+            ->select('permission')
+            ->innerJoin(
+                'user_role',
+                'user_role.role_id = role_permission.role_id'
+            )
+            ->where([
+                'user_role.user_id' => $user->id,
+            ])
+            ->column();
+
+        $permissions = array_merge($permissions, $rolePermissions);
+
+        return array_unique($permissions);
     }
 }
